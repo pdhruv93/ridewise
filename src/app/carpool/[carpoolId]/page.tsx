bf73cef149d3081a2fb1f9c57fc91ddefa45c5f7
pdhruv93 <dhruv.pandey93@gmail.com>
2025-02-24 11:58:33 +0200

    Update 404

--- src/app/carpool/[carpoolId]/page.tsx
+++ src/app/carpool/[carpoolId]/page.tsx
@@ -1,8 +1,31 @@
-export default async function Page({
+import { createClient } from "@/utils/supabase/server";
+import { notFound } from "next/navigation";
+
+export default async function Carpool({
   params,
 }: {
   params: Promise<{ carpoolId: string }>;
 }) {
   const carpoolId = (await params).carpoolId;
+
+  if (!carpoolId) {
+    notFound();
+  }
+
+  const supabase = await createClient();
+  const {
+    data: { user },
+    error,
+  } = await supabase.auth.getUser();
+
+  if (!user || error) {
+    notFound();
+  }
+
+  const { data } = await supabase.rpc("get_carpools", {
+    logged_in_user_id: user.id,
+    search_text: query,
+  });
+
   return <>This is the details page page {carpoolId}</>;
 }
