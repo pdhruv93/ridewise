8d07bf09fedb3419df948a6ce9df9929055f2f26
pdhruv93 <dhruv.pandey93@gmail.com>
2025-02-25 10:50:46 +0200

    Login route

---
+++ src/middleware.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { createClient } from "./utils/supabase/server";
+
+export async function middleware(request: NextRequest) {
+  const supabase = await createClient();
+  const {
+    data: { user },
+    error,
+  } = await supabase.auth.getUser();
+
+  if (error || !user) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+}
+
+export const config = {
+  matcher: ["/list"],
+};
