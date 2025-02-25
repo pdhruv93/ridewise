ee55ef3fd873fc76b40b6b0772a5fcadfe6bbcc2
pdhruv93 <dhruv.pandey93@gmail.com>
2025-02-25 11:32:25 +0200

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,10 +6,16 @@ import {
   DialogTitle,
 } from "@/components/ui/dialog";
 import { LoginForm } from "@/components/login/login-form";
+import { createClient } from "@/utils/supabase/server";
 
-export default function LoginModal() {
+export default async function LoginModal() {
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
   return (
-    <DialogRoot size="xs" open lazyMount>
+    <DialogRoot size="xs" open={!user} lazyMount>
       <DialogContent>
         <DialogHeader>
           <DialogTitle>Login</DialogTitle>
