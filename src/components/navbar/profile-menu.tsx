473b41198defd5f305c814465305008bb269b894
pdhruv93 <dhruv.pandey93@gmail.com>
2025-02-26 12:54:25 +0200

    Create list dirs

---
+++ src/components/navbar/profile-menu.tsx
@@ -0,0 +1,35 @@
+import {
+  MenuContent,
+  MenuItem,
+  MenuRoot,
+  MenuTrigger,
+} from "@/components/ui/menu";
+import { Avatar } from "@chakra-ui/react";
+import { Logout } from "@/components/login/logout";
+import NextLink from "next/link";
+
+export const UserMenu = () => {
+  return (
+    <MenuRoot positioning={{ placement: "bottom" }}>
+      <MenuTrigger>
+        <Avatar.Root colorPalette="teal">
+          <Avatar.Fallback />
+        </Avatar.Root>
+      </MenuTrigger>
+
+      <MenuContent>
+        <MenuItem value="my_carpool">
+          <NextLink href="/list/my_carpools">My carpools</NextLink>
+        </MenuItem>
+
+        <MenuItem value="carpool_requests" asChild>
+          <NextLink href="/list/requests">Requests</NextLink>
+        </MenuItem>
+
+        <MenuItem value="carpool_requests" color="fg.error">
+          <Logout />
+        </MenuItem>
+      </MenuContent>
+    </MenuRoot>
+  );
+};
