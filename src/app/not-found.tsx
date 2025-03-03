b308e31274de9bd5846042024c563d3653359ae8
pdhruv93 <dhruv.pandey93@gmail.com>
2025-03-03 20:22:02 +0200

    Update not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,3 @@
-import { AbsoluteCenter, Center } from "@chakra-ui/react/center";
 import { Heading } from "@chakra-ui/react/typography";
 import { VStack } from "@chakra-ui/react/stack";
 
@@ -7,19 +6,15 @@ import NextLink from "next/link";
 
 export default function NotFound() {
   return (
-    <AbsoluteCenter w="full">
-      <Center h="100px" w="full">
-        <VStack bgColor="black" p="2" w="full">
-          <Heading size="3xl">
-            The resource you requested does not exists or you dont have enough
-            permissions to access it.
-          </Heading>
+    <VStack align="start" gap="4">
+      <Heading size="xl">
+        The resource you requested does not exists or you dont have enough
+        permissions to access it.
+      </Heading>
 
-          <ChakraLink asChild>
-            <NextLink href="/">Go back</NextLink>
-          </ChakraLink>
-        </VStack>
-      </Center>
-    </AbsoluteCenter>
+      <ChakraLink asChild>
+        <NextLink href="/">Go back</NextLink>
+      </ChakraLink>
+    </VStack>
   );
 }
